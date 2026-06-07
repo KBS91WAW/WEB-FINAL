0db38ede43400ae055731451444e5b9ceac96f8c
KBS91pl <krzysztof.biedasawicki@gmail.com>
2026-06-07 06:35:07 +0200

    Compiled

--- SkillSnap/SkillSnap.Client/obj/Debug/net10.0/SkillSnap.Client.AssemblyInfo.cs
+++ SkillSnap/SkillSnap.Client/obj/Debug/net10.0/SkillSnap.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillSnap.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45fd98a8785613869788d80d35804c581908b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillSnap.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillSnap.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
